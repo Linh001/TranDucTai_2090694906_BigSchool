c3c5eaddb6ee3dc33a12a66579dc08bce7bb271d
Administrator <Administrator@10PC504>
2023-03-09 11:03:07 +0700

    Add project files.

---
+++ TranDucTai_2090694906_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranDucTai_2090694906_BigSchool.MvcApplication" Language="C#" %>
---
+++ TranDucTai_2090694906_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TranDucTai_2090694906_BigSchool.Startup))]
+namespace TranDucTai_2090694906_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
